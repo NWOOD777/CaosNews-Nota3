a1b5ca64009903aa7aa4944c74164fe73cc3b2b4
NWOOD777 <jo.riquelmet@duocuc.cl>
2024-06-27 22:59:01 -0400

    lol

--- CaosNews/settings.py
+++ CaosNews/settings.py
@@ -55,7 +55,7 @@ INSTALLED_APPS = [
     'rest_framework', 
     'axes',
     'captcha',
-    'cloudinary',
+    'cloudinary_storage',
 ]
 
 
@@ -177,7 +177,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
