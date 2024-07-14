f850dca4a95092b752928c9a696359cc1da7acdb
NWOOD777 <jo.riquelmet@duocuc.cl>
2024-07-14 18:49:18 -0400

    asdas

--- core/urls.py
+++ core/urls.py
@@ -37,13 +37,10 @@ urlpatterns = [
 
     #Baucher
     path('voucher/', voucher, name="voucher"),
-<<<<<<< HEAD
 
     # Password reset
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-=======
->>>>>>> 974e573ee1894c7ddfb3789a33240ed611937ca6
 ]
